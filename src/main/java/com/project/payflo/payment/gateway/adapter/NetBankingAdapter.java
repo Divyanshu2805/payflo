01d381826ea3779650edf461d60c68a7a8b61354
Divyanshu Agrahari <divyanshu11235@gmail.com>
2025-11-03 12:48:44 +0530

    feat: wire netbanking adapter to payment processor router

--- src/main/java/com/project/payflo/payment/gateway/adapter/NetBankingAdapter.java
+++ src/main/java/com/project/payflo/payment/gateway/adapter/NetBankingAdapter.java
@@ -1,8 +1,12 @@
 package com.project.payflo.payment.gateway.adapter;
 
+import com.project.payflo.common.enums.PaymentMethod;
 import com.project.payflo.payment.gateway.PaymentAdapter;
 import com.project.payflo.payment.gateway.dto.PaymentRequest;
 import com.project.payflo.payment.gateway.dto.PaymentResult;
+import com.project.payflo.payment.processor.PaymentProcessorRouter;
+import com.project.payflo.payment.processor.dto.PaymentProcessorRequest;
+import com.project.payflo.payment.processor.dto.PaymentProcessorResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -12,9 +16,36 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class NetBankingAdapter implements PaymentAdapter {
 
+    private final PaymentProcessorRouter paymentProcessorRouter;
+
     @Override
     public PaymentResult initiate(PaymentRequest request) {
-        // TODO: integrate with a real net banking processor
-        return null;
+        log.info("Initiate Payment with NetBankingAdapter, paymentId: {}", request.paymentId());
+
+        try {
+            PaymentProcessorRequest paymentProcessorRequest = PaymentProcessorRequest.nonCard(
+                    request.paymentId(),
+                    PaymentMethod.NETBANKING,
+                    request.amount(),
+                    request.methodDetails()
+            );
+
+            PaymentProcessorResponse paymentProcessorResponse =
+                    paymentProcessorRouter.charge(paymentProcessorRequest);
+
+            return switch (paymentProcessorResponse) {
+                case PaymentProcessorResponse.Failure failure ->
+                        new PaymentResult.Failure(failure.errorCode(), failure.errorDescription());
+
+                case PaymentProcessorResponse.Pending pending ->
+                        new PaymentResult.Pending(pending.processorReference());
+
+                case PaymentProcessorResponse.Success success -> new PaymentResult.Success(success.bankReference());
+
+            };
+        } catch(Exception e) {
+            log.warn("NetBanking failed, paymentId: {}", request.paymentId());
+            return new PaymentResult.Failure("NBK_FAILED", e.getMessage());
+        }
     }
 }
