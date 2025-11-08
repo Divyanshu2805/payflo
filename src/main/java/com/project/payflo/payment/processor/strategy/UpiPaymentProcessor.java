14b528f7279eeffcf8c883a5889a9977c6cb214e
Divyanshu Agrahari <divyanshu11235@gmail.com>
2025-11-08 17:28:36 +0530

    feat: implement mock upi processor logic

--- src/main/java/com/project/payflo/payment/processor/strategy/UpiPaymentProcessor.java
+++ src/main/java/com/project/payflo/payment/processor/strategy/UpiPaymentProcessor.java
@@ -1,5 +1,6 @@
 package com.project.payflo.payment.processor.strategy;
 
+import com.project.payflo.common.util.RandomizerUtil;
 import com.project.payflo.payment.processor.PaymentProcessor;
 import com.project.payflo.payment.processor.dto.PaymentProcessorRequest;
 import com.project.payflo.payment.processor.dto.PaymentProcessorResponse;
@@ -10,6 +11,20 @@ public class UpiPaymentProcessor implements PaymentProcessor {
 
     @Override
     public PaymentProcessorResponse charge(PaymentProcessorRequest request) {
-        return null;
+        final String VPA_CODE_FAIL = "fail@okaxis";
+
+        Object vpa = request.methodDetails() != null ? request.methodDetails().get("vpa") : null;
+        String vpaCode = vpa != null ? vpa.toString() : null;
+
+        if (VPA_CODE_FAIL.equals(vpaCode)) {
+            return new PaymentProcessorResponse.Failure("UPI_REJECTED",
+                    "Banked rejected the transaction registration"
+            );
+        }
+
+        String processorRef = "UPI_PROCESSOR_"+ RandomizerUtil.randomBase64(16);
+        String bankref = "BANK_REF" + RandomizerUtil.randomBase64(16);
+
+        return new PaymentProcessorResponse.Success(processorRef, bankref);
     }
 }
